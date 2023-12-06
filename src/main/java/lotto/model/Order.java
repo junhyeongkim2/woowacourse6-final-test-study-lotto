31bcb092864fda658309256515e779c0f0756b91
jun <ggprgrkjh@naver.com>
2023-12-06 18:36:20 +0900

    refactor(order): 에러 메세지 상수화 및 매직넘버 제거

--- src/main/java/lotto/model/Order.java
+++ src/main/java/lotto/model/Order.java
@@ -1,6 +1,9 @@
 package lotto.model;
 
 public class Order {
+
+    private final int DIVDE_STANDARD = 1000;
+    private final String DIVDE_ERROR_MESSAGE = "[ERROR] 로또 구입 금액은 1000원 단위로 나누어떨어져야 합니다. 다시 입력해 주세요.";
     private final int buyAmount;
 
     public Order(int buyAmount) {
@@ -9,7 +12,7 @@ public class Order {
     }
 
     public int calculateBuyQuanity() {
-        return buyAmount / 1000;
+        return buyAmount / DIVDE_STANDARD;
     }
 
     public int getBuyAmount() {
@@ -17,8 +20,8 @@ public class Order {
     }
 
     private void validateIsDivideByThousand(int buyAmount) {
-        if (buyAmount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 나누어떨어져야 합니다. 다시 입력해 주세요.");
+        if (buyAmount % DIVDE_STANDARD != 0) {
+            throw new IllegalArgumentException(DIVDE_ERROR_MESSAGE);
         }
     }
 
