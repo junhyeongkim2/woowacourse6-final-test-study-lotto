bd1ce6544a893966a50ff7ab409037e78afd5d7b
jun <ggprgrkjh@naver.com>
2023-12-06 16:11:31 +0900

    feat(outputview): 발행한 로또 번호 출력 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,15 @@
 package lotto.view;
 
+import lotto.model.Lottos;
+
 public class OutputView {
 
-    public static void printLottoQuanity(int quanity){
-        System.out.println(quanity+"개를 구매했습니다.");
+    public static void printLottoQuanity(int quanity) {
+        System.out.println(quanity + "개를 구매했습니다.");
+    }
+
+    public static void printLottoNumbers(Lottos lottos) {
+        lottos.getLottos().stream().forEach(lotto -> System.out.println(lotto));
     }
 
 
