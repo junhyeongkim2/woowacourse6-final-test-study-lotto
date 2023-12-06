5f046b94c21eebebf9b2928c9a66e564ae8e3688
jun <ggprgrkjh@naver.com>
2023-12-06 18:40:54 +0900

    refactor(winningnumbers): 매직넘버 제거 및 에러 메세지 상수화

--- src/main/java/lotto/model/WinningNumbers.java
+++ src/main/java/lotto/model/WinningNumbers.java
@@ -5,13 +5,18 @@ import java.util.stream.Collectors;
 
 public class WinningNumbers {
 
+    private final String DUPLICATE_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 당첨번호에 중복된 숫자가 발생했습니다. 다시 입력해 주세요.";
+    private final String INVALID_RANGE_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 당첨번호에 범위를 벗어난 숫자가 발생했습니다. 다시 입력해 주세요.";
+    private final String DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 중복된 보너스 번호가 발생했습니다. 다시 입력해 주세요.";
+    private final int START_NUMBER = 1;
+    private final int END_NUMBER = 45;
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         validateDuplicateWinningNumbers(winningNumbers);
         validateInValidRangeNumbers(winningNumbers);
-        validateDuplicateBonusNumber(winningNumbers,bonusNumber);
+        validateDuplicateBonusNumber(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
@@ -24,21 +29,21 @@ public class WinningNumbers {
         return lottoNumber == bonusNumber;
     }
 
-    public void validateDuplicateWinningNumbers(List<Integer> winningNumbers) {
+    private void validateDuplicateWinningNumbers(List<Integer> winningNumbers) {
         if (winningNumbers.stream().distinct().collect(Collectors.toList()).size() != winningNumbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨번호에 중복된 숫자가 발생했습니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(DUPLICATE_WINNING_NUMBER_ERROR_MESSAGE);
         }
     }
 
-    public void validateInValidRangeNumbers(List<Integer> winningNumbers) {
-        if (!winningNumbers.stream().allMatch(number -> number >= 1 && number <= 45)) {
-            throw new IllegalArgumentException("[ERROR] 당첨번호에 중복된 숫자가 발생했습니다. 다시 입력해 주세요.");
+    private void validateInValidRangeNumbers(List<Integer> winningNumbers) {
+        if (!winningNumbers.stream().allMatch(number -> number >= START_NUMBER && number <= END_NUMBER)) {
+            throw new IllegalArgumentException(INVALID_RANGE_WINNING_NUMBER_ERROR_MESSAGE);
         }
     }
 
     public void validateDuplicateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
         if (winningNumbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("[ERROR] 중복된 보너스 번호가 발생했습니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE);
         }
     }
 
