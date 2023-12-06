24b1cd54f572bfab7e95b283230b4f7c37a40026
jun <ggprgrkjh@naver.com>
2023-12-06 19:02:08 +0900

    refactor(splitor): 매직넘버 제거

--- src/main/java/lotto/util/Splitor.java
+++ src/main/java/lotto/util/Splitor.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Splitor {
+    private static final String SPLIT_STRING = ",";
+
     public static List<Integer> splitWiningNumbers(String winningNumbers) {
-        return Arrays.stream(winningNumbers.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        return Arrays.stream(winningNumbers.split(SPLIT_STRING)).map(Integer::parseInt).collect(Collectors.toList());
     }
 }
