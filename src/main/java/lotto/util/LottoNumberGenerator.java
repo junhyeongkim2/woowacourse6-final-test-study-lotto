56f5001d2edff5910a089c881704ef6bbaceb5ba
jun <ggprgrkjh@naver.com>
2023-12-06 19:01:12 +0900

    refactor(lottonumbergenerator): 매직넘버 제거

--- src/main/java/lotto/util/LottoNumberGenerator.java
+++ src/main/java/lotto/util/LottoNumberGenerator.java
@@ -7,9 +7,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LottoNumberGenerator {
+
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+    private static final int LIMIT = 6;
+
     public static List<Integer> generateLottoNumbers() {
         List<Integer> numbers = new ArrayList<>();
-        numbers.addAll(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        numbers.addAll(Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, LIMIT));
         sortNumbers(numbers);
         return numbers;
     }
