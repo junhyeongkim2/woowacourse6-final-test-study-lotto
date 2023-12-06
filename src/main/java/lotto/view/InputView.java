17b450673befb351e56507052e5ab92530eb9650
jun <ggprgrkjh@naver.com>
2023-12-06 17:50:17 +0900

    feat(inputview): 당첨번호 형식 검사 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputView {
 
@@ -14,6 +16,7 @@ public class InputView {
     public static String readWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
+        validateWinningNumbersForm(input);
         return input;
     }
 
@@ -23,11 +26,19 @@ public class InputView {
         return input;
     }
 
-    public static void validateReadBuyAmount(String input) {
+    private static void validateReadBuyAmount(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 구입금액이 입력되었습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static void validateWinningNumbersForm(String input) {
+        Pattern pattern = Pattern.compile("^\\d+(,\\d+)*$");
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 당첨번호가 입력되었습니다. 다시 입력해 주세요.");
         }
     }
 
