604423adb18f44d388cc04c41919bd9d1a49f049
jun <ggprgrkjh@naver.com>
2023-12-06 17:35:08 +0900

    feat(order): 로또 구입 금액 1000원 단위로 나누어 떨어지는지 검사

--- src/main/java/lotto/model/Order.java
+++ src/main/java/lotto/model/Order.java
@@ -4,6 +4,7 @@ public class Order {
     private final int buyAmount;
 
     public Order(int buyAmount) {
+        validateIsDivideByThousand(buyAmount);
         this.buyAmount = buyAmount;
     }
 
@@ -13,6 +14,12 @@ public class Order {
 
     public int getBuyAmount() {
         return buyAmount;
+    }
+
+    private void validateIsDivideByThousand(int buyAmount) {
+        if (buyAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 나누어떨어져야 합니다. 다시 입력해 주세요.");
+        }
     }
 
 
